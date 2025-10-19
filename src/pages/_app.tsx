baa58504b6fdec83d9237f1dc1732d92182be5d3
JoaChP <joanenrique2703@gmail.com>
2025-10-19 16:07:17 -0600

    fix(layout): wrap pages with AppShell so Sidebar shows on pages/*

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app'
 import '../../app/globals.css'
+import { AppShell } from '../components/layout/app-shell'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppShell>
+      <Component {...pageProps} />
+    </AppShell>
+  )
 }
