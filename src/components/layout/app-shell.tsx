1a57d9baae5c1adfdf260fd7a85de9e2fb2bc937
JoaChP <joanenrique2703@gmail.com>
2025-10-25 23:47:29 -0600

    fix: increase bottom padding in AppShell to prevent content overlap with TabBar

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -13,7 +13,7 @@ export const AppShell = memo(function AppShell({ children }: AppShellProps) {
     <div className="min-h-screen bg-zinc-50 dark:bg-zinc-900">
       <TopBar />
       <Sidebar />
-      <main className="pt-16 pb-20 lg:pl-64 lg:pb-8">
+      <main className="pt-16 pb-24 lg:pl-64 lg:pb-8">
         <div className="container mx-auto px-4 py-6">
           {children}
         </div>
